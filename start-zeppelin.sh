2b0ce866164fac1f7b3f05aa708666719e7fd3ad
Slavo Ng <slavo.ng99@gmail.com>
2018-11-20 11:16:19 +0100

    DSP-44 make sure env variables for user and group are set

--- start-zeppelin.sh
+++ start-zeppelin.sh
@@ -30,7 +30,10 @@ replace_env_config log4j.properties
 replace_env_config hive-site.xml
 
 # add zeppelin group if not exists
-if getent group $ZEPPELIN_PROCESS_GROUP_NAME; then
+if [ -z "$ZEPPELIN_PROCESS_GROUP_NAME" ]; then
+  echo "Environment variable ZEPPELIN_PROCESS_GROUP_NAME required, but not set, exiting ..."
+  exit
+elif getent group $ZEPPELIN_PROCESS_GROUP_NAME; then
   echo "Group $ZEPPELIN_PROCESS_GROUP_NAME already exists"
 else
   echo "Group $ZEPPELIN_PROCESS_GROUP_NAME does not exist, creating it with gid=$ZEPPELIN_PROCESS_GROUP_ID"
@@ -38,7 +41,10 @@ else
 fi
 
 # add zeppelin user if not exists
-if id -u $ZEPPELIN_PROCESS_USER_NAME 2>/dev/null; then
+if [ -z "$ZEPPELIN_PROCESS_USER_NAME" ]; then
+  echo "Environment variable $ZEPPELIN_PROCESS_USER_NAME required, but not set, exiting ..."
+  exit
+elif id -u $ZEPPELIN_PROCESS_USER_NAME 2>/dev/null; then
   echo "User $ZEPPELIN_PROCESS_USER_NAME already exists"
 else
   echo "User $ZEPPELIN_PROCESS_USER_NAME does not exist, creating it with uid=$ZEPPELIN_PROCESS_USER_ID"
